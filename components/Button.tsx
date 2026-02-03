0732f6efbb9dac4dcaa99fe4991446e06aa92c6b
Artyom 7evenUp <sheludeshev.artyom@mail.ru>
2026-02-03 04:07:37 +0300

    improved `Button` component: - added `isLoading` animation - added Haptics Feedback on press event - change `bg` and `scale` on press event

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,18 +1,76 @@
-import { ComponentProps } from "react"
-import { StyleSheet, Pressable, Text } from "react-native"
+import { ComponentProps, useEffect } from "react"
+import {
+  StyleSheet,
+  Pressable,
+  Text,
+  PressableStateCallbackType,
+} from "react-native"
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  withRepeat,
+  Easing,
+} from "react-native-reanimated"
+import * as Haptics from "expo-haptics"
+
+import { Loader } from "lucide-react-native"
 
 export default function Button({
   label,
   icon,
+  style,
+  isLoading,
+  onPress,
   ...props
 }: {
   label: string
   icon?: React.ReactNode
+  isLoading?: boolean
 } & ComponentProps<typeof Pressable>) {
+  const rotation = useSharedValue(0)
+
+  useEffect(() => {
+    rotation.value = isLoading
+      ? withRepeat(
+          withTiming(360, {
+            duration: 800,
+            easing: Easing.linear,
+          }),
+          -2,
+          false,
+        )
+      : 0
+  }, [isLoading])
+
+  const animatedRotationStyles = useAnimatedStyle(() => ({
+    transform: [{ rotate: `${rotation.value}deg` }],
+  }))
+
   return (
-    <Pressable style={styles.pressable} {...props}>
-      {icon && icon}
-      <Text style={styles.label}>{label}</Text>
+    <Pressable
+      style={(state: PressableStateCallbackType) => [
+        styles.pressable,
+        state.pressed && styles.pressable_pressed,
+        typeof style === "function" ? style(state) : style,
+      ]}
+      onPress={(event) => {
+        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Soft)
+        if (onPress) onPress(event)
+      }}
+      disabled={isLoading}
+      {...props}
+    >
+      {isLoading ? (
+        <Animated.View style={animatedRotationStyles}>
+          <Loader size={24} color="white" />
+        </Animated.View>
+      ) : (
+        <>
+          {icon && icon}
+          <Text style={styles.label}>{label}</Text>
+        </>
+      )}
     </Pressable>
   )
 }
@@ -27,6 +85,10 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     gap: 8,
     backgroundColor: "#56395B",
+  },
+  pressable_pressed: {
+    backgroundColor: "#452E49",
+    transform: [{ scale: 0.98 }],
   },
   label: {
     color: "#fff",
