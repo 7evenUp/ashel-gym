83860cf71e082aacd05b9769942498acee5bd4bd
Artyom 7evenUp <sheludeshev.artyom@mail.ru>
2026-04-14 18:56:48 +0300

    moved muscle group images to separate folder

--- constants/muscleGroupImages.ts
+++ constants/muscleGroupImages.ts
@@ -3,27 +3,27 @@ export const muscleGroupImages: Record<
   Record<"img" | "blurhash", any>
 > = {
   back: {
-    img: require("../assets/images/back_gradient.png"),
+    img: require("../assets/images/muscleGroups/back_gradient.png"),
     blurhash: "KFAn+geRD48^eREnv0jDPX",
   },
   chest: {
-    img: require("../assets/images/chest_gradient.png"),
+    img: require("../assets/images/muscleGroups/chest_gradient.png"),
     blurhash: "KAE_O3OW0000JR,@3Vfk{y",
   },
   biceps: {
-    img: require("../assets/images/biceps_gradient.png"),
+    img: require("../assets/images/muscleGroups/biceps_gradient.png"),
     blurhash: "K5Fhu4OT0000=ZXn00=w.m",
   },
   triceps: {
-    img: require("../assets/images/triceps_gradient.png"),
+    img: require("../assets/images/muscleGroups/triceps_gradient.png"),
     blurhash: "K8D]MZ5400DPD~a_00t7_M",
   },
   legs: {
-    img: require("../assets/images/legs_gradient.png"),
+    img: require("../assets/images/muscleGroups/legs_gradient.png"),
     blurhash: "K39*YfDF00%T*84gX2V^aj",
   },
   shoulders: {
-    img: require("../assets/images/shoulders_gradient.png"),
+    img: require("../assets/images/muscleGroups/shoulders_gradient.png"),
     blurhash: "K5ECd-oz0000WBtm02oJ_N",
   },
 }
